d7d36f9eac06c976a0d25fdaaa55af11dae6baf2
Saurav Mishra <saurav24242424@gmail.com>
2024-12-14 00:13:09 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import jsonServer from 'json-server';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'data/cities.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
